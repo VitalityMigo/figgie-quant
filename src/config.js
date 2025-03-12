abc663e3fd8a6d9510a78e19dff0b6e276ae3b36
VitalityMigo <catwalk7th@gmail.com>
2025-03-12 18:25:10 +0100

    Commit 2

--- src/config.js
+++ src/config.js
@@ -1,6 +1,19 @@
 const gameplay = {
-    common_suit_card: 12,
-    target_suit_card: [8,10],
-    suits_range: [8,10,10,12],
+    suits_range: [8, 10, 10, 12],
     suits_name: ['hearts', 'diamonds', 'clubs', 'spades'],
+}
+
+const data = {
+    suits: 4,
+    prior: 1 / suits,
+    cards : 40,
+    hand : 10,
+    common : 12,
+    uncommon : 10,
+    non_common : cards - common,
+}
+
+module.exports = {
+    gameplay,
+    data,
 }
---
+++ src/static/initial.js
@@ -0,0 +1,5 @@
+const math = require('mathjs');
+
+function compute_simple_range() {
+    //https://github.com/SeanAriel/Bayesian-Inference/blob/master/figgie/Figgie_Strategy.ipynb
+}
